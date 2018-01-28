3470f2630b50ae64b5dec826b16cc884fca12012
Will Essilfie <wke2102@columbia.edu>
2018-01-28 10:28:48 -0500

    import os
    needed if user chooses to use environment variables

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import random
 from flask import Flask, request
 from pymessenger.bot import Bot
-
+import os 
 app = Flask(__name__)
 ACCESS_TOKEN = 'ACCESS_TOKEN'   #ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 VERIFY_TOKEN = 'VERIFY_TOKEN'   #VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
